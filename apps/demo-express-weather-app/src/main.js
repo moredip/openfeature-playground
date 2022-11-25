4615196d01a0fcfbdebea6d69ae033950f306c5f
Pete Hodgson <git@thepete.net>
2022-11-25 13:51:42 -0800

    working express app

--- apps/demo-express-weather-app/src/main.js
+++ apps/demo-express-weather-app/src/main.js
@@ -1,19 +1,24 @@
 import * as path from 'path'
 import * as express from 'express'
 import 'hbs'
+import { allLocations, locationBySlug } from './locations';
 
 
 const app = express()
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.static(path.join(__dirname, 'assets')))
+
 app.get('/', (req, res) => {
   res.redirect('/seattle')
 })
 
-app.get('/:location', (req, res) => {
-  const location = req.params['location']
-  res.render( 'weather', {location})
+app.get('/:slug', (req, res) => {
+  const slug = req.params['slug']
+  const location = locationBySlug(slug)
+  const locations = allLocations(slug)
+  res.render( 'weather', {location,locations})
 })
 
 const port = process.env.port || 3333
